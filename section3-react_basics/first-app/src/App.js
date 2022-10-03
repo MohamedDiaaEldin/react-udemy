44fedd9cafdf71ccc8b3a8bf6e0bbc9f319e038f
MohamedDiaaEldin <mdiaan404@gmail.com>
2022-10-03 17:06:44 +0200

    work with arrays and add filter to the app

--- section3-react_basics/first-app/src/App.js
+++ section3-react_basics/first-app/src/App.js
@@ -3,40 +3,48 @@ import Expenses from "./components/expenses/Expense";
 import NewExpense from "./components/NewExpense/NewExpense";
 import { useState } from "react";
 
+const dummyExpenses = [
+  {
+    id:"e1",
+    title: "car Insurance",
+    amount: 379,
+    date: new Date(2021, 2, 28),
+  },
+  {
+    id:"e2",
+    title: "apple laptop",
+    amount: 10000.5,
+    date: new Date(2022, 3, 1),
+  },
+  {
+    id:"e3",
+    title: "smart watch",
+    amount: 600,
+    date: new Date(2020, 2, 6),
+  },
+];
 function App() {
 
-  const [filterYear, setFilterYear] = useState('2020')
-
-  const [expenses, setExpenses] = useState([
-    {
-      title: "car Insurance",
-      amount: 379,
-      date: new Date(2021, 2, 28),
-    },
-    {
-      title: "apple laptop",
-      amount: 10000.5,
-      date: new Date(2022, 3, 1),
-    },
-    {
-      title: "smart watch",
-      amount: 600,
-      date: new Date(2020, 2, 6),
-    },
-  ]);
+  const [expenses, setExpenses] = useState(dummyExpenses);
+  const [filterYear, setFilterYear] = useState("2020");
 
   const filterExpenses = (year) => {
-    setFilterYear(year);
+    setFilterYear(year);    
   };
 
   const addExpense = (data) => {
-    setExpenses([...expenses, data]);
+    // clean way to update state that depends prvious state
+    setExpenses((prv) => [data, ...prv]);    
   };
   return (
     <div>
       <NewExpense addExpense={addExpense} />
       <div className="expenses">
-        <Expenses selectedYear={filterYear} filterExpenses={filterExpenses} expenses={expenses} />
+        <Expenses
+          selectedYear={filterYear}
+          filterExpenses={filterExpenses}
+          expenses={expenses}
+        />
       </div>
     </div>
   );
--- section3-react_basics/first-app/src/components/expenses/Expense.js
+++ section3-react_basics/first-app/src/components/expenses/Expense.js
@@ -2,10 +2,17 @@ import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
 import FilterExpenses from "./FilterExpenses";
 const Expenses = ({ expenses, filterExpenses, selectedYear }) => {
+  const filteredExpenese = expenses.filter(
+    (expense) => expense.date.getFullYear().toString() === selectedYear
+  );
+
   return (
     <Card>
-      <FilterExpenses selectedYear={selectedYear} filterExpenses={filterExpenses} />
-      {expenses.map((item) => (
+      <FilterExpenses
+        selectedYear={selectedYear}
+        filterExpenses={filterExpenses}
+      />
+      {filteredExpenese.map((item) => (
         <ExpenseItem
           key={item.title}
           title={item.title}
